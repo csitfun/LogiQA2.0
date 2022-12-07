6556dafaccfac332f733afc5286c81890b5b1633
Hanmeng Liu <62553364+csitfun@users.noreply.github.com>
2022-12-07 18:45:30 +0800

    comment if _has_sklearn:
    a condition that causes import error

--- logiqa2nli/utils_nli.py
+++ logiqa2nli/utils_nli.py
@@ -70,7 +70,6 @@ def convert_examples_to_features(examples, tokenizer,
             example.text_b,
             add_special_tokens=True,
             max_length=max_length,
-            return_token_type_ids=True
         )
         input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
 
@@ -870,76 +869,76 @@ def is_sklearn_available():
     return _has_sklearn
 
 
-if _has_sklearn:
+#if _has_sklearn:
 
-    def simple_accuracy(preds, labels):
-        return (preds == labels).mean()
+def simple_accuracy(preds, labels):
+    return (preds == labels).mean()
 
-    def acc_and_f1(preds, labels):
-        acc = simple_accuracy(preds, labels)
-        f1 = f1_score(y_true=labels, y_pred=preds)
-        return {
-            "acc": acc,
-            "f1": f1,
-            "acc_and_f1": (acc + f1) / 2,
-        }
+def acc_and_f1(preds, labels):
+    acc = simple_accuracy(preds, labels)
+    f1 = f1_score(y_true=labels, y_pred=preds)
+    return {
+        "acc": acc,
+        "f1": f1,
+        "acc_and_f1": (acc + f1) / 2,
+    }
 
-    def pearson_and_spearman(preds, labels):
-        pearson_corr = pearsonr(preds, labels)[0]
-        spearman_corr = spearmanr(preds, labels)[0]
-        return {
-            "pearson": pearson_corr,
-            "spearmanr": spearman_corr,
-            "corr": (pearson_corr + spearman_corr) / 2,
-        }
+def pearson_and_spearman(preds, labels):
+    pearson_corr = pearsonr(preds, labels)[0]
+    spearman_corr = spearmanr(preds, labels)[0]
+    return {
+        "pearson": pearson_corr,
+        "spearmanr": spearman_corr,
+        "corr": (pearson_corr + spearman_corr) / 2,
+    }
 
-    def compute_metrics(task_name, preds, labels):
-        assert len(preds) == len(labels)
-        if task_name == "cola":
-            return {"mcc": matthews_corrcoef(labels, preds)}
-        elif task_name == "cood":
-            return {"confusion matrix": confusion_matrix(preds, labels), "mcc": matthews_corrcoef(labels, preds), "f1 score": acc_and_f1(preds, labels)}
-        elif task_name == "sst-2":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "mrpc":
-            return acc_and_f1(preds, labels)
-        elif task_name == "sts-b":
-            return pearson_and_spearman(preds, labels)
-        elif task_name == "qqp":
-            return acc_and_f1(preds, labels)
-        elif task_name == "mnli":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "mnli-mm":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "qnli":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "rte":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "wnli":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "hans":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "scitail":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "snli":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "qa2nli":
-            return {"confusion matrix": confusion_matrix(preds, labels), "mcc": matthews_corrcoef(labels, preds), "f1 score": acc_and_f1(preds, labels)}
-        elif task_name == "anli":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "pnli":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "qood":
-            return {"acc": simple_accuracy(preds, labels)}           
-        else:
-            raise KeyError(task_name)
+def compute_metrics(task_name, preds, labels):
+    assert len(preds) == len(labels)
+    if task_name == "cola":
+        return {"mcc": matthews_corrcoef(labels, preds)}
+    elif task_name == "cood":
+        return {"confusion matrix": confusion_matrix(preds, labels), "mcc": matthews_corrcoef(labels, preds), "f1 score": acc_and_f1(preds, labels)}
+    elif task_name == "sst-2":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "mrpc":
+        return acc_and_f1(preds, labels)
+    elif task_name == "sts-b":
+        return pearson_and_spearman(preds, labels)
+    elif task_name == "qqp":
+        return acc_and_f1(preds, labels)
+    elif task_name == "mnli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "mnli-mm":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "qnli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "rte":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "wnli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "hans":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "scitail":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "snli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "qa2nli":
+        return {"confusion matrix": confusion_matrix(preds, labels), "mcc": matthews_corrcoef(labels, preds), "f1 score": acc_and_f1(preds, labels)}
+    elif task_name == "anli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "pnli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "qood":
+        return {"acc": simple_accuracy(preds, labels)}           
+    else:
+        raise KeyError(task_name)
 
-    def xnli_compute_metrics(task_name, preds, labels):
-        assert len(preds) == len(labels)
-        if task_name == "xnli":
-            return {"acc": simple_accuracy(preds, labels)}
-        else:
-            raise KeyError(task_name)
+def xnli_compute_metrics(task_name, preds, labels):
+    assert len(preds) == len(labels)
+    if task_name == "xnli":
+        return {"acc": simple_accuracy(preds, labels)}
+    else:
+        raise KeyError(task_name)
 
 
 
